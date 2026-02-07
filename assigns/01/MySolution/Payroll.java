2fbdbe4bb19e7baed403976e460d07b3969c2dcf
Shuwan Zhao <shuwanzhao@Mac.ht.home>
2026-02-07 16:53:09 -0500

    put into mysolution

---
+++ assigns/01/MySolution/Payroll.java
@@ -0,0 +1,90 @@
+
+public class Payroll {
+    public static final int INITIAL_MAXIMUM_SIZE = 1024;
+    private Employee[] people;
+    private int maximum_size;
+    private  int current_size;
+
+    public Payroll() {
+	/* your code */
+    this.maximum_size= INITIAL_MAXIMUM_SIZE;
+    this.current_size=0;
+    this.people = new Employee[maximum_size];
+    }
+    public int size(){
+        return current_size;
+    }
+    public void print(){
+        if (current_size == 0) {
+            System.out.println("Payroll is empty");
+        }
+        else{
+            for (int i=0;i<current_size;i++){
+                Employee emp=people[i];
+                System.out.printf("Name: %s, ID: %d, Salary: %.2f\n",emp.name, emp.ID, emp.salary);
+            }
+        }
+
+    }
+    public void add_employee(Employee newbie) {
+	/* your code */
+    if (current_size >= maximum_size) {
+        expandArray();
+    }
+    people[current_size] = newbie;
+    current_size++;
+    }
+    
+    private void expandArray() {
+        int newSize = maximum_size * 2;
+        Employee[] newArray = new Employee[newSize];
+    
+        for (int i = 0; i < current_size; i++) {
+            newArray[i] = people[i];
+        }
+    
+        people = newArray;
+        maximum_size = newSize;
+    }
+    
+
+    public void remove_employee(int i) throws EmployeeIndexException {
+	/* your code */
+    people[i] = people[current_size - 1];
+    people[current_size - 1] = null;
+    current_size--;
+
+    }
+    
+    public int find_employee(String name) throws EmployeeNotFoundException {
+	/* your code */
+    for (int i = 0; i < current_size; i++) {
+        if (people[i].name.equals(name)) {
+            return i;
+        }
+    }
+    throw new EmployeeNotFoundException();
+    }
+
+    public void copy_payroll(Payroll source) {
+	/* your code */
+    for (int i = 0; i < current_size; i++) {
+        people[i] = null;
+    }
+    current_size = 0;
+    add_payroll(source);
+    }
+
+    public void add_payroll(Payroll source) {
+	/* your code */
+    while (current_size + source.current_size > maximum_size) {
+        expandArray();
+    }
+    for (int i = 0; i < source.current_size; i++) {
+        people[current_size] = source.people[i];
+        current_size++;
+    }
+    }
+
+
+}
---
+++ assigns/02/MySolution/Assign02_02.java
@@ -0,0 +1,76 @@
+<<<<<<< HEAD
+=======
+import java.util.Arrays;
+>>>>>>> upstream/main
+
+public class Assign02_02 {
+    /*
+      HX-2025-02-13: 10 points
+      Recursion is a fundamental concept in programming.
+      However, the support for recursion in Java is very limited.
+      Nontheless, we will be making extensive use of recursion in
+      this class.
+     */
+
+    /*
+    // This is a so-called iterative implementation:
+    public static <T extends Comparable<T> > int indexOf(T[] a, T key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            final int mid = lo + (hi - lo) / 2;
+	    final int sign = key.compareTo(a[mid]);
+            if      (sign < 0) hi = mid - 1;
+            else if (sign > 0) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+    */
+    public static <T extends Comparable<T> > int indexOf(T[] a, T key) {
+	// Please give a recursive implementation of 'indexOf' that is
+	// equivalent to the above one
+<<<<<<< HEAD
+        int lo = 0;
+        int hi = a.length - 1;
+        return indexOfplus(a, key,lo,hi);
+    }
+    public static <T extends Comparable<T> > int indexOfplus(T[] a, T key, int lo, int hi) {
+        // Please give a recursive implementation of 'indexOf' that is
+        // equivalent to the above one
+            if (lo>hi){
+                return -1;
+            }
+            final int mid = lo + (hi - lo) / 2;
+            final int sign = key.compareTo(a[mid]);
+            if (sign < 0){
+                return indexOfplus(a, key, lo, mid-1);
+            }
+            else if (sign>0) {
+                return indexOfplus(a, key, mid+1, hi);
+            }
+            else{
+                return mid;
+            }
+        }
+
+
+
+    public static void main(String[] argv) {
+	// Please write some testing code for your implementation of 'indexOf'
+    Integer[] nums = {10, 20, 30, 40, 50};
+        System.out.println(indexOf(nums, 30));
+        System.out.println(indexOf(nums, 15));
+
+        String[] words = {"apple", "banana", "cherry"};
+        System.out.println(indexOf(words, "banana"));
+        System.out.println(indexOf(words, "pear"));
+=======
+    }
+
+    public void main(String[] argv) {
+	// Please write some testing code for your implementation of 'indexOf'
+>>>>>>> upstream/main
+    }
+}
