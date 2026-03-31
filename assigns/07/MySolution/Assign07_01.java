20a9eb037039e7e6f3ddad8686694e5ca5dc27b8
shuwan1208 <shervan@bu.edu>
2026-03-31 15:44:43 -0400

    07

---
+++ assigns/07/MySolution/Assign07_01.java
@@ -0,0 +1,26 @@
+import Library00.LnStrm.*;
+
+import java.util.function.ToIntBiFunction;
+
+public class Assign07_01 {
+    public static <T>
+    LnStrm<T> mergeLnStrm(LnStrm<LnStrm<T>> fxss, ToIntBiFunction<T, T> cmpr) {
+        return new LnStrm<T>(
+            () -> {
+                LnStcn<LnStrm<T>> cxss = fxss.eval0();
+                while (cxss.consq()) {
+                    LnStrm<T> fxs = cxss.hd();
+                    LnStcn<T> cxs = fxs.eval0();
+                    if (cxs.consq()) {
+                        return new LnStcn<T>(
+                            cxs.hd(),
+                            LnStrmSUtil.m2erge0(cxs.tl(), mergeLnStrm(cxss.tl(), cmpr), cmpr)
+                        );
+                    }
+                    cxss = cxss.tl().eval0();
+                }
+                return new LnStcn<T>();
+            }
+        );
+    }
+}
---
+++ assigns/07/MySolution/Assign07_02.java
@@ -0,0 +1,101 @@
+import Library00.LnStrm.*;
+import Library00.FnTuple.*;
+
+public class Assign07_02 {
+    private static long cubeSum(FnTupl2<Integer, Integer> xy) {
+        return cubeSum(xy.sub0, xy.sub1);
+    }
+
+    private static long cubeSum(Integer x, Integer y) {
+        long lx = x.longValue();
+        long ly = y.longValue();
+        return lx * lx * lx + ly * ly * ly;
+    }
+
+    private static LnStrm<Integer> intFrom(int start) {
+        return new LnStrm<Integer>(
+            () -> new LnStcn<Integer>(start, intFrom(start + 1))
+        );
+    }
+
+    private static LnStrm<Integer> ramanujanNumbersFrom(LnStrm<FnTupl2<Integer, Integer>> pairs) {
+        return new LnStrm<Integer>(
+            () -> {
+                LnStcn<FnTupl2<Integer, Integer>> cpairs = pairs.eval0();
+                if (cpairs.nilq()) {
+                    return new LnStcn<Integer>();
+                }
+
+                FnTupl2<Integer, Integer> first = cpairs.hd();
+                long sum = cubeSum(first);
+                LnStrm<FnTupl2<Integer, Integer>> rest = cpairs.tl();
+                boolean repeated = false;
+
+                while (true) {
+                    LnStcn<FnTupl2<Integer, Integer>> crest = rest.eval0();
+                    if (crest.nilq()) {
+                        return repeated ? new LnStcn<Integer>((int) sum, new LnStrm<Integer>()) : new LnStcn<Integer>();
+                    }
+
+                    FnTupl2<Integer, Integer> next = crest.hd();
+                    long nextSum = cubeSum(next);
+                    if (nextSum == sum) {
+                        repeated = true;
+                        rest = crest.tl();
+                    } else {
+                        if (repeated) {
+                            return new LnStcn<Integer>((int) sum, ramanujanNumbersFrom(new LnStrm<FnTupl2<Integer, Integer>>(() -> crest)));
+                        } else {
+                            first = next;
+                            sum = nextSum;
+                            rest = crest.tl();
+                        }
+                    }
+                }
+            }
+        );
+    }
+
+    private static <T> void printFirstN(LnStrm<T> fxs, int n) {
+        LnStrm<T> cur = fxs;
+        int i = 0;
+        while (i < n) {
+            LnStcn<T> cxs = cur.eval0();
+            if (cxs.nilq()) {
+                break;
+            }
+            System.out.println(cxs.hd());
+            cur = cxs.tl();
+            i += 1;
+        }
+    }
+
+    public static
+    LnStrm<Integer>
+    ramanujanNumbers() {
+        return ramanujanNumbersFrom(cubeSumOrderedIntegerPairs());
+    }
+
+    public static
+    LnStrm<FnTupl2<Integer, Integer>>
+    cubeSumOrderedIntegerPairs() {
+        return Assign07_01.mergeLnStrm(
+            LnStrmSUtil.map0(
+                intFrom(1),
+                (x) -> LnStrmSUtil.map0(
+                    intFrom(x),
+                    (y) -> new FnTupl2<Integer, Integer>(x, y)
+                )
+            ),
+            (p1, p2) -> Long.compare(cubeSum(p1), cubeSum(p2))
+        );
+    }
+
+    public static void main(String[] args) {
+        System.out.println("First 15 cube-sum ordered pairs:");
+        printFirstN(cubeSumOrderedIntegerPairs(), 15);
+
+        System.out.println("First 5 Ramanujan numbers:");
+        printFirstN(ramanujanNumbers(), 5);
+    }
+}
